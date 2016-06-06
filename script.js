8afaf5ebe77c9265a934a1ef809e002ab687bc5e
Brennan Gambling <brennan.gambling@gmail.com>
2016-06-05 21:45:27 -0400

    Fixed spelling error.

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function drawSquares() {
 drawSquares();
 
 
-alert("This is 3D X's and O's. Each 2D X's and O's board represent a layer of the 3D X's and O's cube with the left most being the top and the right most being the bottom. To best play the game think of the cube as 2D X's and O's from the top of the cuber, the front of the cube, and the side of the cube.");
+alert("This is 3D X's and O's. Each 2D X's and O's board represent a layer of the 3D X's and O's cube with the left most being the top and the right most being the bottom. To best play the game think of the cube as 2D X's and O's from the top of the cube, the front of the cube, and the side of the cube.");
 
 
 
